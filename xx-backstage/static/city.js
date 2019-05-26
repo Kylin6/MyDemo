73f31d00a9899444e9d44e43bcce100073898e17
Kylin6 <826729970@qq.com>
2019-05-26 20:13:20 +0800

    xx后台系统

---
+++ xx-backstage/static/city.js
@@ -0,0 +1,7 @@
+const commonCityOptions = ['安徽', '北京', '福建', '甘肃', '广东', '广西', '贵州', '海南', '河北', '河南', '黑龙江',
+  '湖北', '湖南', '吉林', '江苏', '江西', '辽宁', '内蒙', '宁夏', '青海', '山东', '山西', '陕西', '上海', '四川', '天津', '云南', '浙江', '重庆'];
+const extraordinaryCityOptions = ['西藏', '新疆', '台湾', '香港', '澳门', '中国其他']
+export {
+  commonCityOptions,
+  extraordinaryCityOptions
+}
